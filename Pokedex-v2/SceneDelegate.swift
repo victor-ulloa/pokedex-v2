305221fa381152ae92b697a7ff4c9e35140a7061
Victor Ulloa <>
2024-03-31 18:17:35 -0400

    tab bar implemented

--- Pokedex-v2/SceneDelegate.swift
+++ Pokedex-v2/SceneDelegate.swift
@@ -13,10 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+            window?.windowScene = windowScene
+            window?.makeKeyAndVisible()
+
+            let tabbar = UITabBarController()
+            
+            let firstVC = UINavigationController(rootViewController: ViewController())
+
+            
+        firstVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"),selectedImage: UIImage(systemName: "magnifyingglass"))
+
+            tabbar.viewControllers = [firstVC]
+
+            window?.rootViewController = tabbar
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
